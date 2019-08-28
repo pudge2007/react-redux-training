2b6e249f3df680a3959ffb05d3a4ccea31563247
Barys Iuchanka <Barys.Iuchanka@itechart-group.com>
2019-08-28 12:16:54 +0300

    make pure components

--- frontend/src/modules/films/pages/FilmPage/components/Film/index.js
+++ frontend/src/modules/films/pages/FilmPage/components/Film/index.js
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { PureComponent } from "react";
 
 import Rating from "../../../../components/Raiting";
 import { styles } from "./styles";
 
-const Film = ({ film }) => {
-  console.log(film);
-  return (
-    <div style={styles.wrapper}>
-      <div style={styles.film}>
-        <div className="row">
-          <div className="col-3">
-            <img src={film.imageURI} style={styles.poster} alt="" />
-          </div>
-          <div className="col-9">
-            <div style={styles.titleWrapper}>
-              <h1 style={styles.title}>{film.title}</h1>
-              <Rating value={film.rating} />
+class Film extends PureComponent {
+  render() {
+    const { film } = this.props;
+    return (
+      <div style={styles.wrapper}>
+        <div style={styles.film}>
+          <div className="row">
+            <div className="col-3">
+              <img src={film.imageURI} style={styles.poster} alt="" />
             </div>
-            <p>{film.description}</p>
+            <div className="col-9">
+              <div style={styles.titleWrapper}>
+                <h1 style={styles.title}>{film.title}</h1>
+                <Rating value={film.rating} />
+              </div>
+              <p>{film.description}</p>
+            </div>
           </div>
         </div>
+        <div className="comments">
+          <h2 style={{ marginBottom: 20 }}>Комментарии (12)</h2>
+        </div>
       </div>
-      <div className="comments">
-        <h2 style={{ marginBottom: 20 }}>Комментарии (12)</h2>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Film;
--- frontend/src/modules/films/pages/FilmsList/components/FilmsList/index.js
+++ frontend/src/modules/films/pages/FilmsList/components/FilmsList/index.js
@@ -1,22 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, PureComponent } from "react";
 
 import FilmsListItem from "../FilmsListItem";
 
-const FilmsList = ({ films }) => {
-  return (
-    <Fragment>
-      <h1 style={{ margin: 20 }}>Список фильмов</h1>
-      <ul className="list-group">
-        {films.map(film => (
-          <FilmsListItem
-            key={film.id}
-            classes="list-group-item list-group-item-action"
-            film={film}
-          />
-        ))}
-      </ul>
-    </Fragment>
-  );
-};
+class FilmsList extends PureComponent {
+  render() {
+    const { films } = this.props;
+    return (
+      <Fragment>
+        <h1 style={{ margin: 20 }}>Список фильмов</h1>
+        <ul className="list-group">
+          {films.map(film => (
+            <FilmsListItem
+              key={film.id}
+              classes="list-group-item list-group-item-action"
+              film={film}
+            />
+          ))}
+        </ul>
+      </Fragment>
+    );
+  }
+}
 
 export default FilmsList;
